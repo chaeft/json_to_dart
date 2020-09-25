db2962c6824996469c5111861f890136609719f1
hongry <hongry@hongryui-Macmini.local>
2020-09-25 14:09:27 +0900

    수정내역
    서버 만듬

---
+++ example/json2.dart
@@ -0,0 +1,51 @@
+import 'dart:io';
+import 'dart:convert';
+
+import "package:path/path.dart" show dirname, join, normalize;
+import '../lib/json_to_dart.dart';
+
+Future main() async {
+  // #docregion bind
+  var server = await HttpServer.bind(
+    InternetAddress.loopbackIPv4,
+    4040,
+  );
+  
+  // #enddocregion bind
+  print('Listening on localhost:${server.port}');
+
+  // #docregion listen
+  await for (HttpRequest request in server) {
+    if (request.method != 'POST' || request.headers.contentType?.mimeType != 'application/json' /*1*/) {
+      request.response.write("¯\\_(ツ)_/¯");
+      await request.response.close();
+      return;
+    }
+
+    try {
+      String content = await utf8.decoder.bind(request).join();
+      print('content - ${content}');
+      var data = jsonDecode(content) as Map;
+      if (data['className'] == null || data['className'].length < 1 || data['jsonData'] == null) {
+        request.response.write('{"success": false, "message": "empty parameter"}');
+        await request.response.close();
+        return;
+      }
+
+      final classGenerator = new ModelGenerator(data['className']);
+      DartCode dartCode = classGenerator.generateDartClasses(JsonEncoder().convert(data['jsonData']));
+
+      request.response.write(dartCode.code);
+      await request.response.close();
+    } catch (e) {
+      print(e);
+      request.response
+          ..statusCode = HttpStatus.internalServerError
+          ..write('Exception during file I/O: $e.');
+      await request.response.close();
+      return;
+    }
+    
+  }
+  // #enddocregion listen
+}
